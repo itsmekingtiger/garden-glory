3d78e635a5aa1858ae9b134ff345b7ec39a4ae54
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-12-25 23:07:08 +0900

    refactor: PlantListItem 분리

--- lib/views/plants_page.dart
+++ lib/views/plants_page.dart
@@ -25,72 +25,7 @@ class PlantsPage extends ConsumerWidget {
           child: ListView.builder(
             itemCount: plants.length,
             itemBuilder: (context, index) {
-              final mostRecentLog = plants[index].mostRecentLog();
-
-              return GestureDetector(
-                onTap: () =>
-                    Navigator.pushNamed(context, '/plant_detail', arguments: PlantDetailPageArgs(plants[index].id)),
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                backgroundImage: plants[index].profileImage == null
-                                    ? null
-                                    : FileImage(File(plants[index].profileImage!)),
-                              ),
-                              // name, species, last event
-                              HSpace.md,
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      plants[index].name,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
-                                    ),
-                                    VSpace.xs,
-                                    Text(
-                                      mostRecentLog == null
-                                          ? '아직 기록이 없어요'
-                                          : dateAgo(mostRecentLog.createdAt, kEndOfToday),
-                                      style: Theme.of(context).textTheme.bodySmall,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              TextButton(
-                                style: ButtonStyle(
-                                  visualDensity: VisualDensity.compact,
-                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[300]!),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context).pushNamed(
-                                    PlantLogEditPage.pageUrl,
-                                    arguments: {'plant': plants[index]},
-                                  );
-                                  // write log
-                                },
-                                child: Text('글쓰기', style: Theme.of(context).textTheme.bodySmall),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
+              return _PlantListItem(plant: plants[index]);
             },
           ),
         ),
@@ -99,44 +34,67 @@ class PlantsPage extends ConsumerWidget {
   }
 }
 
-class BrownActionButton extends StatelessWidget {
-  const BrownActionButton({
+class _PlantListItem extends StatelessWidget {
+  const _PlantListItem({
     Key? key,
-    required this.callback,
-    required this.child,
-    this.icon,
+    required this.plant,
   }) : super(key: key);
 
-  final VoidCallback callback;
-  final Widget child;
-  final Icon? icon;
+  final Plant plant;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Feedback.forTap(context);
-        callback();
-      },
-      child: SizedBox(
-        // height: 100,
+      onTap: () => Navigator.pushNamed(context, '/plant_detail', arguments: PlantDetailPageArgs(plant.id)),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(10),
+        ),
         child: Row(
           children: [
             Expanded(
               child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
+                margin: EdgeInsets.all(10),
                 child: Row(
                   children: [
-                    Expanded(
-                      child: Container(margin: EdgeInsets.all(10), child: child),
+                    CircleAvatar(
+                      backgroundImage: plant.profileImage == null ? null : FileImage(File(plant.profileImage!)),
                     ),
-                    Container(
-                      margin: EdgeInsets.all(10),
-                      child: icon,
+                    // name, species, last event
+                    HSpace.md,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            plant.name,
+                            style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          VSpace.xs,
+                          Text(
+                            plant.mostRecentLog() == null
+                                ? '아직 기록이 없어요'
+                                : dateAgo(plant.mostRecentLog()!.createdAt, kEndOfToday),
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ],
+                      ),
+                    ),
+                    TextButton(
+                      style: ButtonStyle(
+                        visualDensity: VisualDensity.compact,
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[300]!),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                          PlantLogEditPage.pageUrl,
+                          arguments: {'plant': plant},
+                        );
+                        // write log
+                      },
+                      child: Text('글쓰기', style: Theme.of(context).textTheme.bodySmall),
                     ),
                   ],
                 ),

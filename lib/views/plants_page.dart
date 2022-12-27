a79b7ff93b361675502cf5b8511fcb2e6be5e5e6
cmsong <dlcmaldmlzldxkdlrj@gmail.com>
2022-12-27 21:41:28 +0900

    feat: extract ZoomOutAnimationWrapper widget

--- lib/views/plants_page.dart
+++ lib/views/plants_page.dart
@@ -25,7 +25,12 @@ class PlantsPage extends ConsumerWidget {
           child: ListView.builder(
             itemCount: plants.length,
             itemBuilder: (context, index) {
-              return _PlantListItem(plant: plants[index]);
+              return ZoomOutAnimationWrapper(
+                child: PlantListItem(
+                  isPressed: false,
+                  plant: plants[index],
+                ),
+              );
             },
           ),
         ),
@@ -34,74 +39,154 @@ class PlantsPage extends ConsumerWidget {
   }
 }
 
-class _PlantListItem extends StatelessWidget {
-  const _PlantListItem({
+class ZoomOutAnimationWrapper extends StatefulWidget {
+  const ZoomOutAnimationWrapper({
     Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<ZoomOutAnimationWrapper> createState() => _ZoomOutAnimationWrapperState();
+}
+
+class _ZoomOutAnimationWrapperState extends State<ZoomOutAnimationWrapper> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  bool isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 100));
+    _animation = Tween<double>(begin: 1, end: 0.9).animate(_controller);
+    _controller.reset();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void pressed(event) {
+    _controller.forward();
+    setState(() {
+      isPressed = true;
+    });
+  }
+
+  void canclePressed() {
+    _controller.reverse();
+    setState(() {
+      isPressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: CurvedAnimation(
+        parent: _animation,
+        curve: Interval(0, 1, curve: Curves.easeOut),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        child: Listener(
+          onPointerDown: pressed,
+          onPointerUp: (event) => canclePressed(),
+          onPointerCancel: (event) => canclePressed(),
+          child: withPressedFilter(),
+        ),
+      ),
+    );
+  }
+
+  Widget withPressedFilter() {
+    return isPressed ? ColorFiltered(colorFilter: ColorFilters.darkerAlphaScale, child: widget.child) : widget.child;
+  }
+}
+
+class PlantListItem extends StatelessWidget {
+  const PlantListItem({
+    Key? key,
+    required this.isPressed,
     required this.plant,
   }) : super(key: key);
 
+  final bool isPressed;
   final Plant plant;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, '/plant_detail', arguments: PlantDetailPageArgs(plant.id)),
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(10),
+    final profileImage = plant.profileImage == null ? null : FileImage(File(plant.profileImage!));
+
+    final lastLogDate =
+        plant.mostRecentLog() == null ? '아직 기록이 없어요' : dateAgo(plant.mostRecentLog()!.createdAt, kEndOfToday);
+
+    return TextButton(
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
+        splashFactory: NoSplash.splashFactory,
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: plant.profileImage == null ? null : FileImage(File(plant.profileImage!)),
+        visualDensity: VisualDensity.compact,
+        backgroundColor: MaterialStateProperty.all<Color>(isPressed ? Colors.grey[300]! : Colors.grey[200]!),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushNamed(
+          '/plant_detail',
+          arguments: PlantDetailPageArgs(plant.id),
+        );
+      },
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  CircleAvatar(backgroundImage: profileImage),
+
+                  // name, species, last event
+                  HSpace.md,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          plant.name,
+                          style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        VSpace.xs,
+                        Text(lastLogDate, style: Theme.of(context).textTheme.bodySmall),
+                      ],
                     ),
-                    // name, species, last event
-                    HSpace.md,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            plant.name,
-                            style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
-                          ),
-                          VSpace.xs,
-                          Text(
-                            plant.mostRecentLog() == null
-                                ? '아직 기록이 없어요'
-                                : dateAgo(plant.mostRecentLog()!.createdAt, kEndOfToday),
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
+                  ),
+
+                  // write log button
+                  TextButton(
+                    style: ButtonStyle(
+                      visualDensity: VisualDensity.compact,
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(isPressed ? Colors.grey[200]! : Colors.grey[300]!),
                     ),
-                    TextButton(
-                      style: ButtonStyle(
-                        visualDensity: VisualDensity.compact,
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.grey[300]!),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(
-                          PlantLogEditPage.pageUrl,
-                          arguments: {'plant': plant},
-                        );
-                        // write log
-                      },
-                      child: Text('글쓰기', style: Theme.of(context).textTheme.bodySmall),
-                    ),
-                  ],
-                ),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        PlantLogEditPage.pageUrl,
+                        arguments: {'plant': plant},
+                      );
+                    },
+                    child: Text('글쓰기', style: Theme.of(context).textTheme.bodySmall),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

d182333327c6a7b89b8b8932f03dcd452915aa67
cmsong <cmsong@shinasys.com>
2023-04-11 18:07:23 +0900

    server: init

---
+++ server/src/main/java/dev/balthasar/gardenglory/GardengloryApplication.java
@@ -0,0 +1,13 @@
+package dev.balthasar.gardenglory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardengloryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardengloryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/balthasar/gardenglory/GardengloryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.balthasar.gardenglory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardengloryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
